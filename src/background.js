f9e86c453c70ee05b71a4354ecd9064aa463e500
Mateusz <mateusz.szczupak@webgrove.pl>
2026-05-24 00:56:19 +0200

    Add extension new tab page

--- src/background.js
+++ src/background.js
@@ -1,6 +1,45 @@
-chrome.runtime.onMessage.addListener((message, sender) => {
-  if (!message || message.type !== "VIBE_VIM_CLOSE_TAB") return;
-  if (!sender.tab || typeof sender.tab.id !== "number") return;
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (!message) return;
 
-  chrome.tabs.remove(sender.tab.id);
+  if (message.type === "VIBE_VIM_OPEN_TAB") {
+    openTabNearSender(sender)
+      .then((tab) => sendResponse({ ok: true, tabId: tab.id }))
+      .catch((error) => sendResponse({ ok: false, error: error.message }));
+    return true;
+  }
+
+  if (message.type !== "VIBE_VIM_CLOSE_TAB") return;
+
+  resolveSenderTab(sender).then((tab) => {
+    if (!tab || typeof tab.id !== "number") {
+      sendResponse({ ok: false });
+      return;
+    }
+    chrome.tabs.remove(tab.id).then(() => sendResponse({ ok: true }));
+  }).catch((error) => sendResponse({ ok: false, error: error.message }));
+  return true;
 });
+
+async function openTabNearSender(sender) {
+  const tab = await resolveSenderTab(sender);
+  const createProperties = {};
+
+  if (tab && typeof tab.index === "number") {
+    createProperties.index = tab.index + 1;
+  }
+  if (tab && typeof tab.id === "number") {
+    createProperties.openerTabId = tab.id;
+  }
+
+  return chrome.tabs.create(createProperties);
+}
+
+async function resolveSenderTab(sender) {
+  if (sender.tab) return sender.tab;
+
+  const [activeTab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  });
+  return activeTab || null;
+}
